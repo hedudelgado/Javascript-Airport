a6d59c075b2f3d93bb6602491d2ce9d48613cbbf
hedudelgado <hedu.delgado@gmail.com>
2016-02-17 19:34:14 +0000

    airport nearly finished

--- src/Plane.js
+++ src/Plane.js
@@ -1,6 +1,10 @@
-'use strict';
-
 function Plane(){}
+
 Plane.prototype.land = function(airport){
-	airport.clearForLanding(this);
+  airport.clearForLanding(this);
+  this._location = airport;
+};
+
+Plane.prototype.takeoff = function(airport){
+  this._location.clearForTakeOff('');
 };
